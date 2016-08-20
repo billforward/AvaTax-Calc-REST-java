443184c369cc05464716d3e01b7f495ee2fad1bc
Tom Gardiner <tomg@refme.com>
2016-08-19 17:12:31 -0700

    Tidy up TaxSvc a little

--- src/com/avatax/tax/TaxSvc.java
+++ src/com/avatax/tax/TaxSvc.java
@@ -27,82 +27,35 @@ public class TaxSvc {
 		//Create URL
 		String taxget = svcURL+"/1.0/tax/get";
 
-		//Connect to URL with authorization header, request content.
-		URL url = new URL(taxget);
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		conn.setRequestMethod("POST");
-		conn.setDoOutput(true);
-		conn.setDoInput(true);
-		conn.setUseCaches(false);
-		conn.setAllowUserInteraction(false);
-
-		String encoded = "Basic " + new String(Base64.encodeBase64((accountNum+":"+license).getBytes())); //Create auth content
-		conn.setRequestProperty ("Authorization", encoded); //Add authorization header
-		conn.setRequestProperty("Content-Type",
-				"application/x-www-form-urlencoded");
-
-		ObjectMapper mapper = objectMapper();
-		String content = mapper.writeValueAsString(req);
-		//System.out.println(content);   //Uncomment to see the content of the request object
-		conn.setRequestProperty("Content-Length", Integer.toString(content.length()));
-
-		DataOutputStream wr = new DataOutputStream (conn.getOutputStream ());
-		wr.writeBytes (content);
-		wr.flush ();
-		wr.close ();
-
-		conn.disconnect();
+		HttpURLConnection conn = doRequest(taxget, req);
 
 		if(conn.getResponseCode() != 200) //If we didn't get a success back, print out the error.
 		{
-			GetTaxResult res = mapper.readValue(conn.getErrorStream(), GetTaxResult.class); //Deserializes the response
+			GetTaxResult res = objectMapper().readValue(conn.getErrorStream(), GetTaxResult.class); //Deserializes the response
 			return res;
 		}
 		else //Otherwise, print out the total tax calculated
 		{
-			GetTaxResult res = mapper.readValue(conn.getInputStream(), GetTaxResult.class); //Deserializes the response
+			GetTaxResult res = objectMapper().readValue(conn.getInputStream(), GetTaxResult.class); //Deserializes the response
 			return res;
 		}
 	}
 
 	public CancelTaxResult CancelTax(CancelTaxRequest req) throws IOException {
-
 		//Create URL
 		String taxget = svcURL+"/1.0/tax/cancel";
-		//Connect to URL with authorization header, request content.
-		URL url = new URL(taxget);
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		conn.setRequestMethod("POST");
-		conn.setDoOutput(true);
-		conn.setDoInput(true);
-		conn.setUseCaches(false);
-		conn.setAllowUserInteraction(false);
 
-		String encoded = "Basic " + new String(Base64.encodeBase64((accountNum+":"+license).getBytes())); //Create auth content
-		conn.setRequestProperty ("Authorization", encoded); //Add authorization header
-		conn.setRequestProperty("Content-Type",
-				  "application/x-www-form-urlencoded");
-
-		ObjectMapper mapper = objectMapper();
-		String content = mapper.writeValueAsString(req);
-		//System.out.println(content);			//Uncomment to see the content of the request object
-		conn.setRequestProperty("Content-Length", Integer.toString(content.length()));
-
-		DataOutputStream wr = new DataOutputStream (conn.getOutputStream ());
-		wr.writeBytes (content);
-		wr.flush ();
-		wr.close ();
-
-		conn.disconnect();
+		HttpURLConnection conn = doRequest(taxget, req);
 
 		if(conn.getResponseCode() != 200)   //Note: tax/cancel will return a 200 response even if the document could not be cancelled. Special attention needs to be paid to the ResultCode.
-		{									//If we got a more serious error, print out the error message.
-			CancelTaxResult res = mapper.readValue(conn.getErrorStream(), CancelTaxResult.class);  //Deserialize response
+		{
+            //If we got a more serious error, print out the error message.
+			CancelTaxResult res = objectMapper().readValue(conn.getErrorStream(), CancelTaxResult.class);  //Deserialize response
 			return res;
 		}
 		else
 		{
-			CancelTaxResponse res = mapper.readValue(conn.getInputStream(), CancelTaxResponse.class); //Deserialize response
+			CancelTaxResponse res = objectMapper().readValue(conn.getInputStream(), CancelTaxResponse.class); //Deserialize response
 			return res.CancelTaxResult;
 		}
 	}
@@ -111,30 +64,55 @@ public class TaxSvc {
 		//Create query/url
 		String taxest = svcURL+ "/1.0/tax/" + latitude.toString() + "," + longitude.toString() + "/get?saleamount=" + saleAmount.toString();
 			//Connect to specified URL with authorization header
-		URL url = new URL(taxest);
-		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-		String encoded = "Basic " + new String(Base64.encodeBase64((accountNum+":"+license).getBytes())); //Create auth content
-		conn.setRequestProperty ("Authorization", encoded); //Add authorization header
-		conn.disconnect();
 
-		ObjectMapper mapper = objectMapper();
+		HttpURLConnection conn = doRequest(taxest, null);
 
 		if(conn.getResponseCode() != 200) //If we didn't get a success back, print out the error
 		{
-			GeoTaxResult res = mapper.readValue(conn.getErrorStream(), GeoTaxResult.class); //Deserializes the response object
+			GeoTaxResult res = objectMapper().readValue(conn.getErrorStream(), GeoTaxResult.class); //Deserializes the response object
 			return res;
 		}
-
 		else //Otherwise, print out the validated address.
 		{
-			GeoTaxResult res = mapper.readValue(conn.getInputStream(), GeoTaxResult.class); //Deserializes the response object
+			GeoTaxResult res = objectMapper().readValue(conn.getInputStream(), GeoTaxResult.class); //Deserializes the response object
 			return res;
 		}
 	}
 
+	private HttpURLConnection doRequest(String urlPath, Object req) throws IOException {
+		//Connect to URL with authorization header, request content.
+		URL url = new URL(urlPath);
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		conn.setRequestMethod("POST");
+		conn.setDoOutput(true);
+		conn.setDoInput(true);
+		conn.setUseCaches(false);
+		conn.setAllowUserInteraction(false);
+
+		String encoded = "Basic " + new String(Base64.encodeBase64((accountNum+":"+license).getBytes())); //Create auth content
+		conn.setRequestProperty ("Authorization", encoded); //Add authorization header
+
+		if(req != null) {
+			conn.setRequestProperty("Content-Type",
+					"application/x-www-form-urlencoded");
+
+			String content = objectMapper().writeValueAsString(req);
+			//System.out.println(content);   //Uncomment to see the content of the request object
+			conn.setRequestProperty("Content-Length", Integer.toString(content.length()));
+
+			DataOutputStream wr = new DataOutputStream(conn.getOutputStream());
+			wr.writeBytes(content);
+			wr.flush();
+			wr.close();
+		}
+
+		conn.disconnect();
+		return conn;
+	}
+
     public enum DetailLevel { Tax, Document, Line, Diagnostic };
 
-    public enum DocType 
+    public enum DocType
     { SalesOrder, SalesInvoice, ReturnOrder, ReturnInvoice, PurchaseOrder, PurchaseInvoice, ReverseChargeOrder, ReverseChargeInvoice }
 
 	public GeoTaxResult Ping() throws IOException {
